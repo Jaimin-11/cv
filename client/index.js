fd955dcf20005e3d983998bb8a0edf9243256fd2
jaimin-patel <jaimin3799@gmail.com>
2024-05-27 08:08:51 +0000

    update in index.js

--- client/index.js
+++ client/index.js
@@ -1,4 +1,30 @@
-import { router} from "./router.js";
+// import { router} from "./router.js";
+
+class router {
+    constructor(home_path) {
+        this.home_path = home_path;
+    }
+
+    changeRoute(element_id) {
+        let target_path = this.home_path + "/" + element_id;
+        window.history.replaceState({}, "Jaimin Patel", target_path)
+        this.loadData(target_path);
+    }
+
+    loadData(element_id) {
+        var xhr = typeof XMLHttpRequest != 'undefined' ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+        let file_name = element_id.toLowerCase() + ".html";
+        console.log("filename", file_name)
+        xhr.open('get', 'templates/' + file_name, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                document.getElementById(element_id).innerHTML = xhr.responseText;
+            }
+        }
+        xhr.send();
+    }
+}
+
 
 const home_path = window.location.href;
 console.log(home_path);
@@ -7,7 +33,7 @@ const curr_router = new router(home_path);
 
 console.log("obj", curr_router.home_path);
 
-document.getElementsByClassName("link-tab").addEventListener("click", tabClicked(), false);
+// document.getElementsByClassName("link-tab").addEventListener("onclick", tabClicked(), false);
 
 function tabClicked(element_id){
     console.log("clicked");
