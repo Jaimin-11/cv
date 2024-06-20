cff62936f7c4ec116530aa6726b4e25a1b9761d3
Jaimin-11 <jaiminpatel057@gmail.com>
2024-06-20 22:56:15 +0530

    update

--- client/router.js
+++ client/router.js
@@ -1,21 +1,24 @@
-export class router {
+class router {
     constructor(home_path) {
         this.home_path = home_path;
     }
 
     changeRoute(element_id) {
-        let target_path = this.home_path + "/" + element_id;
+        let target_element = element_id;
+        let target_path = this.home_path;
+        target_path = this.home_path.substring(0, target_path.search("#^#")) + "#^#" + element_id;
         window.history.replaceState({}, "Jaimin Patel", target_path)
-        this.loadData(target_path);
+        this.loadData(target_element);
     }
 
     loadData(element_id) {
         var xhr = typeof XMLHttpRequest != 'undefined' ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-        file_name = element_id.toLowerCase() + ".html";
+        let file_name = element_id.toLowerCase() + ".html";
+
         xhr.open('get', 'templates/' + file_name, true);
         xhr.onreadystatechange = function () {
             if (xhr.readyState == 4 && xhr.status == 200) {
-                document.getElementById(element_id).innerHTML = xhr.responseText;
+                document.getElementById("CONTENT-BOX").innerHTML = xhr.responseText;
             }
         }
         xhr.send();
