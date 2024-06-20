49549fc00f70c6f67001bbb89cad69f4d5a6c65b
Jaimin-11 <jaiminpatel057@gmail.com>
2024-06-20 22:17:30 +0530

    update

--- client/index.js
+++ client/index.js
@@ -1,15 +1,35 @@
-import { router} from "./router.js";
+class router {
+    constructor(home_path) {
+        this.home_path = home_path;
+    }
+
+    changeRoute(element_id) {
+        let target_element = element_id;
+        let target_path = this.home_path;
+        target_path = this.home_path.substring(0, target_path.search("#^#")) + "#^#" + element_id;
+        window.history.replaceState({}, "Jaimin Patel", target_path)
+        this.loadData(target_element);
+    }
+
+    loadData(element_id) {
+        var xhr = typeof XMLHttpRequest != 'undefined' ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+        let file_name = element_id.toLowerCase() + ".html";
+        xhr.open('get', 'templates/' + file_name, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                document.getElementById("CONTENT-BOX").innerHTML = xhr.responseText;
+            }
+        }
+        xhr.send();
+    }
+}
 
 const home_path = window.location.href;
-console.log(home_path);
 
 const curr_router = new router(home_path);
 
-console.log("obj", curr_router.home_path);
-
-document.getElementsByClassName("link-tab").addEventListener("click", tabClicked(), false);
-
 function tabClicked(element_id){
-    console.log("clicked");
     curr_router.changeRoute(element_id);
 }
+
+tabClicked("HOME");
